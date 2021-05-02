95ca6f1801bd4998953a9361b45726a4a4984f70
adelminayi <50608574+adelminayi@users.noreply.github.com>
2021-05-02 12:20:45 +0430

    clean code

--- MongoApp/urls.py
+++ MongoApp/urls.py
@@ -1,13 +1,8 @@
 from django.urls import path
-from .views import home,Contact,MyFormView, download_file
+from .views import MyFormView, download_file
 
 
 urlpatterns = [
-    # path('home/', HomePageView.as_view()),
-    path('home/', home , name='home'),
-    # path('contact/', Contact.as_view() , name='contact'),
     path('', MyFormView.as_view() , name='test'),
     path('download/<str:filename>/', download_file, name='download'),
-    # path('download/<str:filename>//',download_file, name='downlaod'),
-
 ]
